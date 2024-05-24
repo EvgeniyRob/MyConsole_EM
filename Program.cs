ad1110898a15ad31a4a2becf84b16ad2e2a6463e
EvgeniyRob <evgeny.tsyvanin@yandex.ru>
2024-05-24 20:01:16 +0300

    Урок 1.3. Типы данных  24,05,24

--- Program.cs
+++ Program.cs
@@ -10,7 +10,74 @@ namespace MyConsole_EM
     {
         static void Main(string[] args)
         {
+            levels = new List<decimal>();
 
+            WriteLine();
+
+            string str = ReadLine("Введите количество уровней: ");
+
+            contLevels = Convert.ToInt32(str);
+
+            str = ReadLine("Введите верхнюю цену: ");
+
+            priceUp = decimal.Parse(str);
+
+            str = ReadLine("Введите шашг уровня: ");
+
+            StepLevel = decimal.Parse(str);
+
+            WriteLine();
+
+            Console.ReadLine();
         }
+        static List<decimal> levels;
+
+        static decimal priceUp;
+
+        static int contLevels;
+        static decimal StepLevel
+        {
+            get
+            {
+                return stepLevel;
+            }
+            set
+            {
+                if(value <= 100)
+                {
+                    stepLevel = value;
+
+                    decimal priceLevel = priceUp;
+
+                    for (int i = 0; i < contLevels; i++)
+                    {
+                        levels.Add(priceLevel);
+
+                        priceLevel -= stepLevel;
+
+                    }
+                }
+            }
+        }
+        static decimal stepLevel;
+
+
+        static void WriteLine()
+        {
+            Console.WriteLine("Количество элементов в списке: " + levels.Count.ToString());
+
+            for (int i = 0; i < levels.Count; i++)
+            {
+                Console.WriteLine(levels[i]);
+            }
+        }
+        static string ReadLine(string massage)
+        {
+            Console.WriteLine(massage); 
+
+            return Console.ReadLine(); ;
+        }
+
     }
+
 }
